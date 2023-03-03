529684bb30bf1a4521e4b43cab9153f92ed61d7b
a-jester <jesteralex@hotmail.com>
2023-03-03 10:36:39 -0500

    add Check For Duplicate submissions

---
+++ Check-For-Duplicate/README.md
@@ -0,0 +1,17 @@
+# Check for Duplicate
+
+## C++
+[Solution](https://leetcode.com/problems/contains-duplicate/submissions/908384484/)
+
+	Runtime 127 ms
+	Beats 75.20%
+	Memory 57.2 MB
+	Beats 70.43%
+
+## Python
+[Solution](https://leetcode.com/problems/contains-duplicate/submissions/908397425/)
+
+	Runtime 567 ms
+	Beats 58.76%
+	Memory 28.7 MB
+	Beats 58.64%
---
+++ Check-For-Duplicate/Solution.h
@@ -0,0 +1,20 @@
+#include <vector>		// std::vector
+#include <algorithm>    // std::sort
+
+class Solution {
+public:
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+};
